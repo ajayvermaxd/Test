f33aa0dbccce0f82354a4fcc4d618314d2114a33
unknown <studajay3@gmail.com>
2023-06-25 16:01:56 +0530

    added a new file of loops

---
+++ loops.py
@@ -0,0 +1,28 @@
+# index=0
+# while (index < 10):
+#     print("I am a good student")
+#     index = index+1
+
+    #print all the numbers for 1 to 10
+
+# index=0
+# while (index < 10):
+#     index = index+1
+#     print(index)
+
+##
+
+#take input from the user 
+#print all the even number till that number
+#print the sum of all the numbers
+
+InputNumber= int (input("enter the number"))
+index=0
+sum=0
+while (index<=InputNumber):
+        if ( index %2== 0):
+              print(index)
+              sum=sum+index
+        index=index+1
+print("the sum of total even number till the input is",sum)
+
